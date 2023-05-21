880ea2b8c8fc961cc800e52f39f67bafa279fd78
Bruno <brunormarques@live.com.pt>
2023-05-21 22:56:52 +0100

    Started adding support to roads as a 3rd criteria.

--- wps-service/processes/house_searching_simulator.py
+++ wps-service/processes/house_searching_simulator.py
@@ -1,77 +1,69 @@
 import os
 import sys
-# PostGIS sets GDAL_DATA and PROJ_LIB on the system environments to its own folders, and due to that fiona stopped working properly.
-# The next two lines will remove those variables from the environment dictionary. 
-os.environ.pop('GDAL_DATA', None)
-os.environ.pop('PROJ_LIB', None)
-
 import shutil
-
-from enum import IntEnum
-
-from pywps import Process, LiteralInput, ComplexOutput, Format, FORMATS
-from osgeo import gdal
-from osgeo import ogr
-from geo.Geoserver import Geoserver
-
 import subprocess
 import json
 
-# The inputs should be simple inputs, as the inputs are from a form.
-# The first is school minimum distances, and near or far
+# PostGIS sets GDAL_DATA and PROJ_LIB on the system environments to its own folders
+# The next two lines will remove those variables from the environment dictionary to prevent issues.
+os.environ.pop('GDAL_DATA', None)
+os.environ.pop('PROJ_LIB', None)
 
+from enum import IntEnum
+
+# Allows the implementation of a process of a wps service.
+from pywps import Process, ComplexInput, ComplexOutput, Format, FORMATS
+
+# The core of the implemention of a suitability modelling (or a multi-criteria analysis) 
+from osgeo import gdal
+from osgeo import ogr
+
+# Required for this process to be able to send the result of the analysis to geoserver.
+from geo.Geoserver import Geoserver
+
+# This an enum that allows mapping a number (it will come from the frontend as numbers) to a specific type
+# This allows to understand what criteria the user wants to define what's a good distance type from something.
+# For instance if the person wants to be close to something, or far from it, or somewhere where it's not far or near.
 class FunctionType(IntEnum):
     NEAR = 1
     FAR = 2
     MIDPOINT = 3
 
-# My class needs to inherit from the Process class
 class HouseSearchSimulator(Process):
-    BASE_PATH = os.getcwd()
-    DATA_PATH = os.path.join(BASE_PATH, "../data")
-    OUTPUT_PATH = os.path.join(BASE_PATH, "outputs")
-    GDAL_CALC_PATH = os.path.join(os.getcwd(), '.venv/Scripts/gdal_calc.py')
-    USER_SPECIFIC_PATH = ""
+    base_path = os.getcwd()
+    data_path = os.path.join(base_path, "../data")
+    output_path = os.path.join(base_path, "outputs")
+    gdal_calc_path = os.path.join(base_path, '.venv/Scripts/gdal_calc.py')
+    user_specific_output_path = output_path # will be set later after the process starts.
 
     def __init__(self):
-        inputs = [LiteralInput('data', 'data', data_type="string")]
-        outputs = [ComplexOutput('response',
-                                 'Output response', supported_formats=[Format('application/json')])]
+        inputs = [ComplexInput('data', 'data', supported_formats=[Format('application/json')])]
+        outputs = [ComplexOutput('response', 'Output response', supported_formats=[Format('application/json')])]
 
         # Set the attributes
         super(HouseSearchSimulator, self).__init__(
             self._handler,
             identifier='simulator',
             title='Process House Searching Simulator',
-            abstract='TODO',
+            abstract='This process does a multi-criteria analysis to recommend specific locations, depending on the user input',
             inputs=inputs,
-            outputs=outputs,
-            store_supported=True,
-            status_supported=True
+            outputs=outputs
         )
 
-    def _create_folder(self, path):
-        if os.path.exists(path):
-            return
-
-        # function makedirs allows to create folders and subfolders, unlike mkdir
-        os.makedirs(path)
-
     def _handler(self, request, response):
-        # Here we do the actual geospatial analysis
-        # First step is to define what is necessary
-        # Second step is to derive from the define, meaning, to make sure that any vector data is rasterized and raster data is correct.
-        # 1 - Start with the school distance analysis
-        #     Schools locations are on the database, so we need to retrieve it first with ogr or gdal
-        #     Once locations are retrieved, rasterize them. (Derive)
+        # Here we do the actual geospatial analysis based on:
+        # - Idea: https://desktop.arcgis.com/en/analytics/case-studies/understanding-the-suitability-modeling-workflow.htm
+        # - Example with GDAL: https://spatialthoughts.com/2022/01/31/multi-criteria-overlay-gdal/
 
         # Parse inputs from request
         data = json.loads(request.inputs['data'][0].data)
         criteria = data["Criteria"]
         person_id = data["PersonId"]
         
-        self.USER_SPECIFIC_PATH = os.path.join(self.OUTPUT_PATH, person_id)
-        self._create_folder(self.USER_SPECIFIC_PATH)
+        # Create user specific folder which will be deleted in the end. This will be basically where temporary raster files are used
+        # to create the final suitable locations raster, which will be uploaded to Geoserver.
+        self.user_specific_output_path = os.path.join(self.output_path, person_id)
+        self._create_folder(self.user_specific_output_path)
 
         # Derive
         self.derive()
@@ -87,9 +79,24 @@ class HouseSearchSimulator(Process):
         #TODO: In case of failure in any of the steps before, return False, use a try catch finally.
         out_bytes = json.dumps(json.loads('{"url":"'+ url +'","layer":"' + layer + '"}'))
 
+        # Cleanup raster files that are no longer needed.
+        self._delete_folder(self.user_specific_output_path)
+
         response.outputs['response'].data_format = FORMATS.JSON
         response.outputs['response'].data = out_bytes
         return response
+
+    # Required to create a user specific folder.
+    def _create_folder(self, path):
+        if os.path.exists(path):
+            return
+
+        # function makedirs allows to create folders and subfolders, unlike mkdir
+        os.makedirs(path)
+
+    def _delete_folder(self, path):
+        if os.path.exists(path):
+            shutil.rmtree(path)
     
     def _generate_raster(self, infile, outfile):
         # inspired by http://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#convert-an-ogr-file-to-a-raster
@@ -144,33 +151,41 @@ class HouseSearchSimulator(Process):
             return f"1*(A<{values[0]}) + 1*(A>{values[1]}) + 5*(A>{values[2]})*(A<={values[1]}) + 10*(A>={values[0]})*(A<={values[2]})"
         
     def derive(self):
+        # TODO: These files are always the same, we could verify first if the files already exist to prevent unnecessary processing, but I'll accept the Tech Debt
         # Derive school distance
-        schools_shapefile_location = os.path.join(self.DATA_PATH, "POIEducacao")
-        schools_raster_filename = os.path.join(self.OUTPUT_PATH, "schools.tif")
-        schools_proxmity_raster_filename = os.path.join(self.OUTPUT_PATH, "distance_schools.tif")
+        schools_shapefile_location = os.path.join(self.data_path, "POIEducacao")
+        schools_raster_filename = os.path.join(self.output_path, "schools.tif")
+        schools_proximity_raster_filename = os.path.join(self.output_path, "distance_schools.tif")
         self._generate_raster(schools_shapefile_location, schools_raster_filename)
-        self._compute_proximity(schools_raster_filename, schools_proxmity_raster_filename)
+        self._compute_proximity(schools_raster_filename, schools_proximity_raster_filename)
 
         # Derive metro distance
-        metro_shapefile_location = os.path.join(self.DATA_PATH, "POITransportes")
-        metro_raster_filename = os.path.join(self.OUTPUT_PATH, "metro.tif")
-        metro_proxmity_raster_filename = os.path.join(self.OUTPUT_PATH, "distance_metro.tif")
+        metro_shapefile_location = os.path.join(self.data_path, "POITransportes")
+        metro_raster_filename = os.path.join(self.output_path, "metro.tif")
+        metro_proximity_raster_filename = os.path.join(self.output_path, "distance_metro.tif")
         self._generate_raster(metro_shapefile_location, metro_raster_filename)
-        self._compute_proximity(metro_raster_filename, metro_proxmity_raster_filename)
+        self._compute_proximity(metro_raster_filename, metro_proximity_raster_filename)
+
+        # Derive roads distance
+        roads_shapefile_location = os.path.join(self.data_path, "RedeViaria")
+        roads_raster_filename = os.path.join(self.output_path, "roads.tif")
+        roads_proximity_raster_filename = os.path.join(self.output_path, "distance_roads.tif")
+        self._generate_raster(roads_shapefile_location, roads_raster_filename)
+        self._compute_proximity(roads_raster_filename, roads_proximity_raster_filename)
         return
     
     def transform(self, criteria):
         #TODO: The calculation variables need to come from the WPS inputs
-        distance_school_file = os.path.join(self.OUTPUT_PATH, "distance_schools.tif")
-        distance_school_file_reclass = os.path.join(self.USER_SPECIFIC_PATH, "distance_schools_reclass.tif")
+        distance_school_file = os.path.join(self.output_path, "distance_schools.tif")
+        distance_school_file_reclass = os.path.join(self.user_specific_output_path, "distance_schools_reclass.tif")
         calc = self._calculate_function(criteria[0]["FunctionType"], (20, 15)) #TODO: Not good enough, need to improve, but not important at this stage ...
-        subprocess.call([sys.executable, self.GDAL_CALC_PATH, '-A', distance_school_file, '--outfile', distance_school_file_reclass , f'--calc="{calc}"' ])
+        subprocess.call([sys.executable, self.gdal_calc_path, '-A', distance_school_file, '--outfile', distance_school_file_reclass , f'--calc="{calc}"' ])
 
     	#TODO: The calculation variables need to come from the WPS inputs
-        metro_school_file = os.path.join(self.OUTPUT_PATH, "distance_metro.tif")
-        metro_school_file_reclass = os.path.join(self.USER_SPECIFIC_PATH, "distance_metro_reclass.tif")
+        metro_school_file = os.path.join(self.output_path, "distance_metro.tif")
+        metro_school_file_reclass = os.path.join(self.user_specific_output_path, "distance_metro_reclass.tif")
         calc = self._calculate_function(criteria[1]["FunctionType"], (10, 40, 20)) #TODO: Not good enough, need to improve, but not important at this stage ...
-        subprocess.call([sys.executable, self.GDAL_CALC_PATH, '-A', metro_school_file, '--outfile', metro_school_file_reclass , f'--calc="{calc}"' ])
+        subprocess.call([sys.executable, self.gdal_calc_path, '-A', metro_school_file, '--outfile', metro_school_file_reclass , f'--calc="{calc}"' ])
 
         return distance_school_file_reclass, metro_school_file_reclass
     
@@ -180,7 +195,7 @@ class HouseSearchSimulator(Process):
         currChar = startChar
         values = ()
         calc = ""
-        suitability_filename = os.path.join(self.USER_SPECIFIC_PATH, "suitability.tif")
+        suitability_filename = os.path.join(self.user_specific_output_path, "suitability.tif")
 
         for arg in args:
             values = values + ("-"+currChar, arg[0])
@@ -193,14 +208,14 @@ class HouseSearchSimulator(Process):
         values += ('--overwrite', )
         finalCalc = f"({calc})/{len(args)}"
         
-        subprocess.call([sys.executable, self.GDAL_CALC_PATH, *values , f'--calc="{finalCalc}"' ])
+        subprocess.call([sys.executable, self.gdal_calc_path, *values , f'--calc="{finalCalc}"' ])
         return suitability_filename
     
     def locate(self, suitabilitymap, person_id):
         nodatavalue = '0'
-        suitableregions = os.path.join(self.USER_SPECIFIC_PATH, f"final_suitability_map_{person_id}.tif")
+        suitableregions = os.path.join(self.user_specific_output_path, f"final_suitability_map_{person_id}.tif")
         calc = "0*(A<13)+A*(A>=13)"
-        subprocess.call([sys.executable, self.GDAL_CALC_PATH, '-A', suitabilitymap, '--outfile', suitableregions, f'--calc="{calc}"', '--NoDataValue', nodatavalue, '--overwrite' ])
+        subprocess.call([sys.executable, self.gdal_calc_path, '-A', suitabilitymap, '--outfile', suitableregions, f'--calc="{calc}"', '--NoDataValue', nodatavalue, '--overwrite' ])
         return suitableregions
 
     def submit_to_geoserver(self, suitable_regions, person_id):
